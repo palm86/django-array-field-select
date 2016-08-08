fb699ec97cce20cebe2633c285f7128b63033d7d
Ryan Pineo <ryanpineo@gmail.com>
2016-08-08 15:16:10 -0400

    release v0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='django-array-field-select',
-    version='0.1.2',
+    version='0.1.3',
     description=('A replacement for Django\'s ArrayField with a multiple '
                  'select form field.'),
     long_description=read('README.rst'),
